61fdf5fda14fe0951834eadf9980ae7c6bad3de6
acecrosser <acecrosser@yandex.ru>
2020-11-20 22:55:16 +0300

    Check

--- task_folder/find_script.py
+++ task_folder/find_script.py
@@ -12,8 +12,8 @@ with open('README.md', 'w', encoding='utf-8') as readme:
             schema_data = schema_file.read()
             schema_data = json.loads(schema_data)
         readme.write(f'### Проверка папки по схеме: {schema} \n')
-        readme.write('*' * 100 + '\n')
-        readme.write('\n')
+        # readme.write('*' * 100 + '\n')
+        # readme.write('\n')
         try:
             Draft7Validator.check_schema(schema_data)
         except SchemaError as er:
@@ -24,13 +24,13 @@ with open('README.md', 'w', encoding='utf-8') as readme:
             with open(f'event\\{event}', 'r') as check_file:
                 file = check_file.read()
                 file = json.loads(file)
-            readme.write('*' * 100 + '\n')
-            readme.write(f'Начата проверка файла: {event} \n')
+            # readme.write('*' * 100 + '\n')
+            readme.write(f'Начата проверка файла: {event} \n\n')
             try:
                 validate(instance=file, schema=schema_data)
-                readme.write(f'Файл прошел проверку, ошибок по схеме "{schema}" - не найдено \n')
+                readme.write(f'Файл прошел проверку, ошибок по схеме "{schema}" - не найдено \n\n')
             except ValidationError as er:
-                readme.write(f'Найдена ошибка по схеме {schema}: \n')
+                readme.write(f'Найдена ошибка по схеме {schema}: \n\n')
                 readme.write(str(er))
             readme.write('-' * 100 + '\n')
             readme.write('\n')
