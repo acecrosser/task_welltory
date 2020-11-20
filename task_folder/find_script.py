3b13e69d57c7332d5f2ea73e8a8e9278327c3983
acecrosser <acecrosser@yandex.ru>
2020-11-21 00:28:42 +0300

    final commit

--- task_folder/find_script.py
+++ task_folder/find_script.py
@@ -6,6 +6,22 @@ list_schema = os.listdir('.' + '\\schema')
 list_event = os.listdir('.' + '\\event')
 
 with open('README.md', 'w', encoding='utf-8') as readme:
+    readme.write('### Отчет проверки валидности JSON данных \n'
+                 'Ниже приведен список проверки предоставленных файлов (folder:event) \n\n'
+                 '> Файлы прошедшие проверку идут со статусом: **Прошел проверку** :white_check_mark:\n\n'
+                 '> Файлы с ошибками идут под статусом: **Найдена ошибка** :warning: \n\n'
+                 'Как читать ошибки:\n\n'
+                 'В первой строке ошибки описывается тот параметр который не прошел проверку. К примеру ошибка -  '
+                 '*None is not of type "integer"* - указывает, что в требуемом поле указано **None**, вместо '
+                 'требуемого **integer**. Далее описывается схема параметров предложенной schema-проверки файлов, '
+                 'где описано правило для данного параметра. И последней строкой **On instance** указывается какой '
+                 'именно параметр в файле указан ошибочно. '
+                 'В данном случае, вместе требуемого "целого числа" (перевеод с англ. - integer) в файле указан '
+                 'параметр "None", что означает ничего. Для исправления нам требуется в требуемом поле параметра '
+                 'указать целое число. \n\n Если вам попался ответ **None** в строке **On instance** и первый '
+                 'параметр описывает ошибку как - **None is not of type "object"** в таких случаях файл проверки '
+                 'пуст или в нем указан один единственный параметр - **Null он же None**'
+                 ' \n\n')
 
     for schema in list_schema:
         with open(f'schema\\{schema}', 'r') as schema_file:
@@ -25,10 +41,10 @@ with open('README.md', 'w', encoding='utf-8') as readme:
                 file = check_file.read()
                 file = json.loads(file)
             # readme.write('*' * 100 + '\n')
-            readme.write(f'Начата проверка файла: {event} >>> ')
+            readme.write(f'Проверка файла: {event} >>> ')
             try:
                 validate(instance=file, schema=schema_data)
-                readme.write(f'**Файл прошел проверку** :white_check_mark: \n\n')
+                readme.write(f'**Прошел проверку** :white_check_mark: \n\n')
             except ValidationError as er:
                 readme.write(f'**Найдена ошибка** :warning: \n\n')
                 # print([str(er)])
